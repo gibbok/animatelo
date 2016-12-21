af1ee16b1a173dabd68bc991c644f14db46d8cf8
gibbok <gibbok.coding@gmail.com>
2016-12-21 17:27:12 +0100

    Add animation swing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
     //var logos = document.querySelectorAll('.logo');
     //var logos = document.querySelectorAll('#logo1');
-    var logos = '#logo1';
+    //var logos = '#logo1';
     //var logos = '.logo';
-    //var logos = '#target';
+    var logos = '#target';
 
     //var players = window.animatejs.bounce(logos);
     //var players = window.animatejs.flash(logos);
@@ -12,7 +12,7 @@
     //var players = window.animatejs.shake(logos);
     //var players = window.animatejs.swing(logos);
     //var players = window.animatejs.tada(logos);
-    //var players = window.animatejs.wobble(logos);
+    var players = window.animatejs.wobble(logos);
 
 
     // var players = window.animatejs.bounce(logos, {
---
+++ src/attentionSeekers/swing.js
@@ -0,0 +1,32 @@
+; (function(animatejs) {
+    'use strict';
+    animatejs.swing = function(selector, options) {
+        var keyframeset = [
+            {
+                "transform": "rotate3d(0, 0, 1, 0deg)",
+                "offset": 0
+            },
+            {
+                "transform": "rotate3d(0, 0, 1, 15deg)",
+                "offset": 0.2
+            },
+            {
+                "transform": "rotate3d(0, 0, 1, -10deg)",
+                "offset": 0.4
+            },
+            {
+                "transform": "rotate3d(0, 0, 1, 5deg)",
+                "offset": 0.6
+            },
+            {
+                "transform": "rotate3d(0, 0, 1, -5deg)",
+                "offset": 0.8
+            },
+            {
+                "transform": "rotate3d(0, 0, 1, 0deg)",
+                "offset": 1
+            }
+        ];
+        return animatejs._animate(selector, keyframeset, options);
+    }
+})(window.animatejs = window.animatejs || {});
