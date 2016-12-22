034102c33d2f1d570674d2832be98ab24a984c59
gibbok <gibbok.coding@gmail.com>
2016-12-22 16:25:00 +0100

    Add cleaning

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
     //var logos = document.querySelectorAll('.logo');
     //var logos = document.querySelectorAll('#logo1');
-    //var logos = '#logo1';
+    var logos = '#logo3';
     //var logos = '.logo';
-    var logos = '#target';
+    //var logos = '#target';
 
     //var players = window.animatejs.bounce(logos);
     //var players = window.animatejs.flash(logos);
@@ -16,6 +16,8 @@
     //var players = window.animatejs.jello(logos);
     //var players = window.animatejs.headShake(logos);
     var players = window.animatejs.bounceIn(logos); 
+
+
     
     //var players = window.animatejs.shake('#logos');
 
--- src/attentionSeekers/flash.js
+++ src/attentionSeekers/flash.js
@@ -2,19 +2,19 @@
     'use strict';
     animatejs.flash = function (selector, options) {
         var keyframeset = [{
-            "opacity": "1",
+            "opacity": 1,
             "offset": 0
         }, {
-            "opacity": "0",
+            "opacity": 0,
             "offset": 0.25
         }, {
-            "opacity": "1",
+            "opacity": 1,
             "offset": 0.5
         }, {
-            "opacity": "0",
+            "opacity": 0,
             "offset": 0.75
         }, {
-            "opacity": "1",
+            "opacity": 1,
             "offset": 1
         }];
         return animatejs._animate(selector, keyframeset, options);
