66c0965a23be0e9fe2b788aab1b4421af190f1ab
gibbok <gibbok.coding@gmail.com>
2016-12-24 16:14:26 +0100

    Add cleanup properties

--- src/attentionSeekers/headShake.js
+++ src/attentionSeekers/headShake.js
@@ -3,31 +3,31 @@
     animatejs.headShake = function(selector, options) {
         var keyframeset = [
             {
-                transform: "translateX(0)",
+                transform: 'translateX(0)',
                 offset: 0
             },
             {
-                transform: "translateX(-6px) rotateY(-9deg)",
+                transform: 'translateX(-6px) rotateY(-9deg)',
                 offset: 0.065
             },
             {
-                transform: "translateX(5px) rotateY(7deg)",
+                transform: 'translateX(5px) rotateY(7deg)',
                 offset: 0.185
             },
             {
-                transform: "translateX(-3px) rotateY(-5deg)",
+                transform: 'translateX(-3px) rotateY(-5deg)',
                 offset: 0.315
             },
             {
-                transform: "translateX(2px) rotateY(3deg)",
+                transform: 'translateX(2px) rotateY(3deg)',
                 offset: 0.435
             },
             {
-                transform: "translateX(0)",
+                transform: 'translateX(0)',
                 offset: 0.5
             },
             {
-                transform: "translateX(0)",
+                transform: 'translateX(0)',
                 offset: 1
             }
         ];
--- src/attentionSeekers/jello.js
+++ src/attentionSeekers/jello.js
@@ -3,43 +3,43 @@
     animatejs.jello = function (selector, options) {
         var keyframeset = [
             {
-                transform: "none",
+                transform: 'none',
                 offset: 0
             },
             {
-                transform: "none",
+                transform: 'none',
                 offset: 0.111
             },
             {
-                transform: "skewX(-12.5deg) skewY(-12.5deg)",
+                transform: 'skewX(-12.5deg) skewY(-12.5deg)',
                 offset: 0.222
             },
             {
-                transform: "skewX(6.25deg) skewY(6.25deg)",
+                transform: 'skewX(6.25deg) skewY(6.25deg)',
                 offset: 0.332
             },
             {
-                transform: "skewX(-3.125deg) skewY(-3.125deg)",
+                transform: 'skewX(-3.125deg) skewY(-3.125deg)',
                 offset: 0.444
             },
             {
-                transform: "skewX(1.5625deg) skewY(1.5625deg)",
+                transform: 'skewX(1.5625deg) skewY(1.5625deg)',
                 offset: 0.555
             },
             {
-                transform: "skewX(-0.78125deg) skewY(-0.78125deg)",
+                transform: 'skewX(-0.78125deg) skewY(-0.78125deg)',
                 offset: 0.665
             },
             {
-                transform: "skewX(0.390625deg) skewY(0.390625deg)",
+                transform: 'skewX(0.390625deg) skewY(0.390625deg)',
                 offset: 0.777
             },
             {
-                transform: "skewX(-0.1953125deg) skewY(-0.1953125deg)",
+                transform: 'skewX(-0.1953125deg) skewY(-0.1953125deg)',
                 offset: 0.888
             },
             {
-                transform: "none",
+                transform: 'none',
                 offset: 1
             }
         ];
