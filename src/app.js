586267996ac1b9b9a3d22960281706d5735c2f85
gibbok <gibbok.coding@gmail.com>
2016-12-24 15:41:03 +0100

    Add animation fadeInLeft

--- src/app.js
+++ src/app.js
@@ -27,8 +27,8 @@
     //var players = window.animatejs.bounceOutUp(logos);
     //var players = window.animatejs.fadeIn(logos);
     //var players = window.animatejs.fadeInDown(logos);
-    var players = window.animatejs.fadeInDownBig(logos);
-
+    //var players = window.animatejs.fadeInDownBig(logos);
+    var players = window.animatejs.fadeInLeft(logos);
 
 
     //var players = window.animatejs.shake('#logos');
