b516b8dc4011fcccd1cc0b43fd82c4d40df6cb1d
gibbok <gibbok.coding@gmail.com>
2016-12-25 11:59:54 +0100

    Add animation lightSpeedOut

---
+++ src/lightspeed/lightSpeedOut.js
@@ -0,0 +1,18 @@
+; (function (animatejs) {
+    'use strict';
+    animatejs.lightSpeedOut = function (selector, options) {
+        var keyframeset = [
+            {
+                "transform": "none",
+                "opacity": "1",
+                "offset": 0
+            },
+            {
+                "transform": "translate3d(100%, 0, 0) skewX(30deg)",
+                "opacity": "0",
+                "offset": 1
+            }
+        ];
+        return animatejs._animate(selector, keyframeset, options);
+    }
+})(window.animatejs = window.animatejs || {});
