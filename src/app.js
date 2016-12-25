1486f31988205980f963185b0c48a0b561680b2d
gibbok <gibbok.coding@gmail.com>
2016-12-25 11:29:00 +0100

    Add animation fadeOutUpBig

--- src/app.js
+++ src/app.js
@@ -41,7 +41,8 @@
     //var players = window.animatejs.fadeOutLeftBig(logos);
     //var players = window.animatejs.fadeOutRight(logos);
     //var players = window.animatejs.fadeOutRightBig(logos);
-    var players = window.animatejs.fadeOutUp(logos);
+    //var players = window.animatejs.fadeOutUp(logos);
+    var players = window.animatejs.fadeOutUpBig(logos);
 
 
     //var players = window.animatejs.shake('#logos');
