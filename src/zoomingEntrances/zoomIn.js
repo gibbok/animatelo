ad1e3a8fd31fd7af052a14867485388098327b8a
gibbok <gibbok.coding@gmail.com>
2016-12-26 11:48:42 +0100

    Add animation zoomIn

---
+++ src/zoomingEntrances/zoomIn.js
@@ -0,0 +1,23 @@
+; (function (animatejs) {
+    'use strict';
+    animatejs.zoomIn = function (selector, options) {
+        var keyframeset = [
+            {
+                "opacity": "0",
+                "transform": "scale3d(.3, .3, .3)",
+                "offset": 0
+            },
+            {
+                "opacity": "1",
+                "transform": "none",
+                "offset": 0.5
+            },
+            {
+                "opacity": "1",
+                "transform": "none",
+                "offset": 1
+            }
+        ];
+        return animatejs._animate(selector, keyframeset, options);
+    }
+})(window.animatejs = window.animatejs || {});
