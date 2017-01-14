e18dc6ba0cf280dd2d240b5fa7f7af65a7cef05b
gibbok <gibbok.coding@gmail.com>
2017-01-14 14:22:11 +0100

    Add fix animation

--- src/zoomingExits/zoomOutLeft.js
+++ src/zoomingExits/zoomOutLeft.js
@@ -6,11 +6,10 @@
             {
                 opacity: 1,
                 transform: 'none',
-                transformOrigin: 'left center',
+                'transformOrigin': 'center center',
                 offset: 0
             },
             {
-
                 opacity: 1,
                 transform: 'scale3d(.475, .475, .475) translate3d(42px, 0, 0)',
                 offset: 0.4
