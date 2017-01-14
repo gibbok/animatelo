068d331587abb865b303606c485b421e6a66c0bc
gibbok <gibbok.coding@gmail.com>
2017-01-14 14:53:29 +0100

    Add bug fix animation

--- src/zoomingEntrances/zoomInDown.js
+++ src/zoomingEntrances/zoomInDown.js
@@ -15,7 +15,7 @@
                 animationTimingFunction: 'cubic-bezier(0.175, 0.885, 0.320, 1)',
                 offset: 0.6
             },
-                        {
+            {
                 opacity: 1,
                 transform: 'none',
                 offset: 1
