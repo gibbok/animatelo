ec7e4ccd5a18319bd5bbba74e97fc37c86be4cb7
gibbok <gibbok.coding@gmail.com>
2017-01-14 15:29:00 +0100

    Add fix bug animation

--- src/rotatingEntrances/rotateIn.js
+++ src/rotatingEntrances/rotateIn.js
@@ -4,14 +4,14 @@
     animate.rotateIn = function (selector, options) {
         var keyframeset = [
             {
-                transformOrigin: "center",
-                transform: "rotate3d(0, 0, 1, -200deg)",
+                transformOrigin: 'center',
+                transform: 'rotate3d(0, 0, 1, -200deg)',
                 opacity: 0,
                 offset: 0
             },
             {
-                transformOrigin: "center",
-                transform: "none",
+                transformOrigin: 'center',
+                transform: 'none',
                 opacity: 1,
                 offset: 1
             }
