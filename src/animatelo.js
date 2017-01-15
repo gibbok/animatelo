c651c30e1d4da24875153902eff1c7d900817e21
gibbok <gibbok.coding@gmail.com>
2017-01-15 17:37:45 +0100

    Add new minor version

--- src/animatelo.js
+++ src/animatelo.js
@@ -1,7 +1,7 @@
 /*! Animatelo | The MIT License (MIT) | Copyright (c) 2017 GibboK */
 ; (function (animatelo) {
     'use strict';
-    animatelo.version = '1.0.0';
+    animatelo.version = '1.0.1';
 
     var _defaultOptions = {
         duration: 1000,
