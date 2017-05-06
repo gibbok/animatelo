28cd38c5857ddc965d8156c1295b228aea3a2d34
GibboK <gibbok.coding@gmail.com>
2017-05-06 10:29:32 +0200

    Add fix animations flippers

--- src/flippers/flipInX.js
+++ src/flippers/flipInX.js
@@ -4,29 +4,32 @@
     animatelo.flipInX = function (selector, options) {
         var keyframeset = [
             {
-                transform: 'perspective(400px) rotate3d(1, 0, 0, 90deg)',
-                animationTimingFunction: 'ease-in',
-                opacity: 0,
-                offset: 0
+                "transform": "perspective(400px) rotate3d(1, 0, 0, 90deg)",
+                "opacity": "0",
+                "offset": "0",
+                "easing": "ease-in"
             },
             {
-                transform: 'perspective(400px) rotate3d(1, 0, 0, -20deg)',
-                animationTimingFunction: 'ease-in',
-                offset: 0.4
+                "transform": "perspective(400px) rotate3d(1, 0, 0, -20deg)",
+                "offset": "0.4",
+                "easing": "ease-in"
             },
             {
-                transform: 'perspective(400px) rotate3d(1, 0, 0, 10deg)',
-                opacity: 1,
-                offset: 0.6
+                "transform": "perspective(400px) rotate3d(1, 0, 0, 10deg)",
+                "opacity": "1",
+                "offset": "0.6",
+                "easing": "ease"
             },
             {
-                transform: 'perspective(400px) rotate3d(1, 0, 0, -5deg)',
-                offset: 0.8
+                "transform": "perspective(400px) rotate3d(1, 0, 0, -5deg)",
+                "offset": "0.8",
+                "easing": "ease"
             },
             {
-                transform: 'perspective(400px)',
-                opacity: 1,
-                offset: 1
+                "transform": "perspective(400px)",
+                "opacity": "1",
+                "offset": "1",
+                "easing": "ease"
             }
         ];
         return animatelo._animate(selector, keyframeset, options);
--- src/flippers/flipOutX.js
+++ src/flippers/flipOutX.js
@@ -4,19 +4,22 @@
     animatelo.flipOutX = function (selector, options) {
         var keyframeset = [
             {
-                transform: 'perspective(400px)',
-                opacity: 1,
-                offset: 0
+                "transform": "perspective(400px)",
+                "opacity": "1",
+                "offset": "0",
+                "easing": "ease"
             },
             {
-                transform: 'perspective(400px) rotate3d(1, 0, 0, -20deg)',
-                opacity: 1,
-                offset: 0.3
+                "transform": "perspective(400px) rotate3d(1, 0, 0, -20deg)",
+                "opacity": "1",
+                "offset": "0.3",
+                "easing": "ease"
             },
             {
-                transform: 'perspective(400px) rotate3d(1, 0, 0, 90deg)',
-                opacity: 0,
-                offset: 1
+                "transform": "perspective(400px) rotate3d(1, 0, 0, 90deg)",
+                "opacity": "0",
+                "offset": "1",
+                "easing": "ease"
             }
         ];
         return animatelo._animate(selector, keyframeset, options);
