c88e0155751015191133bb077300ead5d707a4e5
GibboK <gibbok.coding@gmail.com>
2017-05-06 11:16:16 +0200

    Add fix zoomingEntrances

--- src/zoomingEntrances/zoomIn.js
+++ src/zoomingEntrances/zoomIn.js
@@ -4,19 +4,21 @@
     animatelo.zoomIn = function (selector, options) {
         var keyframeset = [
             {
-                opacity: 0,
-                transform: 'scale3d(.3, .3, .3)',
-                offset: 0
+                "opacity": "0",
+                "transform": "scale3d(.3, .3, .3)",
+                "offset": "0",
+                "easing": "ease"
             },
             {
-                opacity: 1,
-                transform: 'none',
-                offset: 0.5
+                "opacity": "1",
+                "offset": "0.5",
+                "easing": "ease"
             },
             {
-                opacity: 1,
-                transform: 'none',
-                offset: 1
+                "opacity": "1",
+                "transform": "none",
+                "offset": "1",
+                "easing": "ease"
             }
         ];
         return animatelo._animate(selector, keyframeset, options);
