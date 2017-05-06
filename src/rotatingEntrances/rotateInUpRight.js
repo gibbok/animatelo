697c7a260fe77ee5d52acf496b92d824701805c3
GibboK <gibbok.coding@gmail.com>
2017-05-06 10:42:01 +0200

    Add fix rotatingEntrances

--- src/rotatingEntrances/rotateInUpRight.js
+++ src/rotatingEntrances/rotateInUpRight.js
@@ -4,16 +4,18 @@
     animatelo.rotateInUpRight = function (selector, options) {
         var keyframeset = [
             {
-                transformOrigin: 'right bottom',
-                transform: 'rotate3d(0, 0, 1, -90deg)',
-                opacity: 0,
-                offset: 0
+                "transformOrigin": "right bottom",
+                "transform": "rotate3d(0, 0, 1, -90deg)",
+                "opacity": "0",
+                "offset": "0",
+                "easing": "ease"
             },
             {
-                transformOrigin: 'right bottom',
-                transform: 'none',
-                opacity: 1,
-                offset: 1
+                "transformOrigin": "right bottom",
+                "transform": "none",
+                "opacity": "1",
+                "offset": "1",
+                "easing": "ease"
             }
         ];
         return animatelo._animate(selector, keyframeset, options);
