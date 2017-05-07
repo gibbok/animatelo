9f1c75644b17eacd71193ee21439a09c5e6bd6ff
GibboK <gibbok.coding@gmail.com>
2017-05-07 08:43:44 +0200

    Add fix headShake

--- src/attentionSeekers/headShake.js
+++ src/attentionSeekers/headShake.js
@@ -6,37 +6,37 @@
             {
                 "transform": 'translateX(0)',
                 "offset": "0",
-                "easing": "ease"
+                "easing": "ease-in-out"
             },
             {
                 "transform": "translateX(-6px) rotateY(-9deg)",
                 "offset": "0.065",
-                "easing": "ease"
+                "easing": "ease-in-out"
             },
             {
                 "transform": "translateX(5px) rotateY(7deg)",
                 "offset": "0.185",
-                "easing": "ease"
+                "easing": "ease-in-out"
             },
             {
                 "transform": "translateX(-3px) rotateY(-5deg)",
                 "offset": "0.315",
-                "easing": "ease"
+                "easing": "ease-in-out"
             },
             {
                 "transform": "translateX(2px) rotateY(3deg)",
                 "offset": "0.435",
-                "easing": "ease"
+                "easing": "ease-in-out"
             },
             {
                 "transform": "translateX(0)",
                 "offset": "0.5",
-                "easing": "ease"
+                "easing": "ease-in-out"
             },
             {
                 "transform": 'translateX(0)',
                 "offset": "1",
-                "easing": "ease"
+                "easing": "ease-in-out"
             }
         ];
         return animatelo._animate(selector, keyframeset, options);
